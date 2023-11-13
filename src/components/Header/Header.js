2dae0e9c77b2e26401307aeec00482cda718d9d6
Vlad Tsoy <potachekkk@yandex.ru>
2023-11-13 17:30:41 +0300

    react router dom i

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './Header.css';
 
 const Header = () => {
   return (
-    <div>
+    <header className='header'>
+        <Link className='header__navLink'>Регистрация</Link>
+        <Link className='header__navLink'>Войти</Link>
+    </header>
 
-    </div>
   );
 };
 

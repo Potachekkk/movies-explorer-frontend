8d2e8f05d8804cdc3267217a06797605203c67e5
Vlad Tsoy <potachekkk@yandex.ru>
2023-11-14 19:34:02 +0300

    feat: добавить роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/saved-movies' element={<SavedMovies />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signup' element={<Register />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+      < Footer />
     </>
   );
 };

23ffeaa4f741b81933d323cdc8a8eced38b91a0f
Vlad Tsoy <potachekkk@yandex.ru>
2023-11-20 18:57:13 +0300

    feat: добавить portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './Portfolio.css';
+import portfolioArrow from '../../../images/portfolio_arrow.png'
 
 const Portfolio = () => {
   return (
-    <div>
-
-    </div>
+    <section className='portfolio'>
+      <h2 className='portfolio__title'>Портфолио</h2>
+      <ul className='portfolio__list'>
+        <li className='portfolio__item'>
+          <a rel='noreferrer' target='_blank' className='portfolio__link' href='https://github.com/Potachekkk/how-to-learn'>
+            <p className='portfolio__info'>Статичный сайт</p>
+            <img className='porfolio__arrow' src={portfolioArrow} alt='Стрелка'/>
+          </a>
+        </li>
+        <li className='portfolio__item'>
+          <a rel='noreferrer' target='_blank' className='portfolio__link' href='https://github.com/Potachekkk/russian-travel'>
+            <p className='portfolio__info'>Адаптивный сайт</p>
+            <img className='porfolio__arrow' src={portfolioArrow} alt='Стрелка'/>
+          </a>
+        </li>
+        <li className='portfolio__item'>
+          <a rel='noreferrer' target='_blank' className='portfolio__link' href='https://github.com/Potachekkk/react-mesto-api-full-gha'>
+            <p className='portfolio__info'>Одностраничное приложение</p>
+            <img className='porfolio__arrow' src={portfolioArrow} alt='Стрелка'/>
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 };
 

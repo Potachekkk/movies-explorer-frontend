f959443863c0a0a9f1c1a67137f106a57caeaae9
Vlad Tsoy <potachekkk@yandex.ru>
2023-12-02 23:22:28 +0300

    README update

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,9 +5,6 @@ import AboutProject from './AboutProject/AboutProject';
 import Techs from './Techs/Techs';
 import AboutMe from './AboutMe/AboutMe';
 import Portfolio from './Portfolio/Portfolio';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer'
-
 
 const Main = () => {
   return (
